57568ac0b6f03134a00656f93bcce04b9269035a
judithhinlung <judithhin@gmail.com>
2024-07-05 16:12:06 -0700

    Added get-current-weather

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@ console.log('Client side javascript file is loaded!')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const sendMyLocation = document.querySelector('#send-my-location')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
@@ -21,6 +22,34 @@ weatherForm.addEventListener('submit', (e) => {
                 messageOne.textContent = data.location
                 messageTwo.textContent = data.forecast
             }
+        })
+    })
+})
+
+sendMyLocation.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.')
+    }
+    sendMyLocation.setAttribute('disabled', 'disabled')
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        const latitude = position.coords.latitude
+        const longitude = position.coords.longitude
+        sendMyLocation.removeAttribute('disabled')
+        messageOne.textContent = 'Loading...'
+        messageTwo.textContent = ''
+
+        fetch('/current-weather?coords=' + latitude + ',' + longitude).then((response) => {
+            response.json().then((data) => {
+                if (data.error) {
+                    messageOne.textContent = data.error
+                } else {
+                    messageOne.textContent = data.location
+                    messageTwo.textContent = data.forecast
+                }
+            })
         })
     })
 })
--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
-const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
-
+const geocode = require('./utils/geocode')
+const reverseGeocode = require('./utils/reverse-geocode')
 const app = express()
-const port = process.env.PORT || 3000 
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -49,37 +49,51 @@ app.get('/weather', (req, res) => {
         })
     }
 
-        geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
             if (error) {
-            return res.send         ({ error })
+                return res.send({ error })
             }
 
-            forecast(latitude, longitude, (error, forecastData) => {
-                if (error) {
-                    return res.send({ error })
-                }
-
-res.send({
-    forecast: forecastData,
-    location,
-    address: req.query.address
-})
-})
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
         })
     })
+})
 
-
-app.get('/products', (req, res) => {
-    if (!req.query.search) {
+app.get('/current-weather', (req, res) => {
+    if (!req.query.coords) {
         return res.send({
-            error: 'You must provide a search term'
+            error: 'location not found'
         })
     }
 
-console.log(req.query.search)
-res.send({
-products: []
-})
+    const coords = req.query.coords.split(',')
+    const latitude = coords[0]
+    const longitude = coords[1]
+
+    forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+            return res.send({ error })
+        }
+        reverseGeocode(latitude, longitude, (error, { address } = {}) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location: address
+            })
+        })
+    })
 })
 
 app.get('/help/*', (req, res) => {
